0203d56f597dbac937cfb9c4f1db069231d85b48
ehehehe95 <53392103+ehehehe95@users.noreply.github.com>
2019-11-01 22:53:19 +0900

    Update DLinkedListMain.c

--- list/Linked-List/DLinkedListMain.c
+++ list/Linked-List/DLinkedListMain.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include "DLinkedList.h"
 #include <Windows.h>
+int WhoIsPrecede(int d1, int d2)
+{
+	if(d1<d2)
+		return 0;
+	else
+		return 1;
+}
+
 int main(void)
 {
 	List list;
 	int data;
 	ListInit(&list);
+
+	SetSortRule(&list,WhoIsPrecede);
 
 	//Instert 5datas in the list
 	Linsert(&list,11); Linsert(&list,11);

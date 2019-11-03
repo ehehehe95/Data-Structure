04fef3df116ba03c7c3befdbc5b7bfc7a92b019f
ehehehe95 <53392103+ehehehe95@users.noreply.github.com>
2019-11-03 20:05:34 +0900

    Add files via upload

---
+++ list/Doubly Linked List/DBLinkedList.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include "DBLinkedList.h"
+#include <stdlib.h>
+void ListInit(List *plist)
+{
+	plist->head=NULL;
+	plist->numOfData=0;
+}
+
+void LInsert(List *plist,Data data)
+{
+	Node *newNode=(Node*)malloc(sizeof(Node));
+	newNode->data=data;
+
+	newNode->next=plist->head;
+	if(plist->head !=0)
+		plist->head->prev=newNode;
+
+	newNode->prev=NULL;
+	plist->head=newNode;
+
+	plist->numOfData++;
+}
+
+int LFirst(List *plist,Data *pdata)
+{
+	if(plist->head->next==NULL)
+		return FALSE;
+
+	plist->cur=plist->head;
+	*pdata=plist->cur->data;
+	return TRUE;
+}
+
+int LNext(List *plist,Data *pdata)
+{
+	if(plist->cur->next==NULL)
+		return FALSE;
+
+	plist->cur=plist->cur->next;
+	*pdata=plist->cur->data;
+	return TRUE;
+}
+
+int LPrevious(List *plist,Data *pdata)
+{
+	if(plist->cur->prev=NULL)
+		return FALSE;
+
+	plist->cur=plist->cur->prev;
+	*pdata=plist->cur->data;
+	return TRUE;
+}
+
+int LCount(List *plist)
+{
+	return plist->numOfData;
+}

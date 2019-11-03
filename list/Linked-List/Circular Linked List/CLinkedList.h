25b946f22cf02ba5ac74e0fccf34f9c825cc48b6
ehehehe95 <53392103+ehehehe95@users.noreply.github.com>
2019-11-03 16:14:24 +0900

    Update CLinkedList.h

--- list/Linked-List/Circular Linked List/CLinkedList.h
+++ list/Linked-List/Circular Linked List/CLinkedList.h
@@ -1,11 +1,10 @@
-#include "Company.h"
 #ifndef __C_LINKED_LIST_H__
 #define __C_LINKED_LIST_H__
 
 #define TRUE	1
 #define FALSE	0
 
-typedef Company* Data;
+typedef int Data;
 
 typedef struct _node
 {
